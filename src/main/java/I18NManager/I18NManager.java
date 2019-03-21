83142f566f67d3d4c61a5182f315b098be6ad6b8
Marioboto3 <marioboto98@gmail.com>
2019-03-21 16:46:21 +0100

    I18NManager creada.

--- src/main/java/I18NManager/I18NManager.java
+++ src/main/java/I18NManager/I18NManager.java
@@ -1,4 +1,18 @@
 package I18NManager;
 
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
 public class I18NManager {
+
+    private static I18NManager instance;
+
+    private I18NManager(){
+    }
+
+    public static I18NManager getInstance(){
+        if(instance==null){instance=new I18NManager();}
+        return instance;
+    }
 }
+
