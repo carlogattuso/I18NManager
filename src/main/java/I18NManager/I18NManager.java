2fdfccf01e15663f8b7d19cbad5d202fbd1e9815
carlogattuso <carlogc46@gmail.com>
2019-03-21 22:49:31 +0100

    Clase actualizada con la carpeta resources

--- src/main/java/I18NManager/I18NManager.java
+++ src/main/java/I18NManager/I18NManager.java
@@ -1,5 +1,31 @@
 package I18NManager;
 
-public class I18NManager {
+import java.util.HashMap;
+import java.util.ResourceBundle;
 
+public class I18NManager {
+    private String l1;
+    private String l2;
+    private HashMap<String, ResourceBundle> data;
+    private static I18NManager instance;
+
+    private I18NManager(){
+        this.data= new HashMap<String, ResourceBundle>();
+    }
+
+    public static I18NManager getInstance(){
+        if(instance==null){instance=new I18NManager();}
+        return instance;
+    }
+
+    public String getlabel(String idioma, String label){
+        ResourceBundle rs = data.get(idioma);
+        if(rs==null)
+        {
+            rs = ResourceBundle.getBundle("I18NManager."+idioma);
+            data.put(idioma,rs);
+        }
+
+        return rs.getString(label);
+    }
 }
